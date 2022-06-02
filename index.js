296ebbaf9a908fb4837147ccbba8743e9b4000f7
jvsr18 <jvsr.99@hotmail.com>
2022-06-01 21:19:08 -0300

    dotenv

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //CONTROLER
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require("path");
 
 app.use(express.urlencoded());
